5c413254c95344a7b13864bee651a97a896aef9a
ukaparyk <ukaparyk@uoguelph.ca>
2021-10-17 02:46:29 -0400

    fixed equals(), added sellBookValue()

--- Stock.java
+++ Stock.java
@@ -40,13 +40,8 @@ public class Stock {
 	public void setBookValue(float price, int quantity){
 		this.bookValue += (price * quantity + 9.99);
 	}
-	
-	//constructor
-	public Stock(int quantity, float price){
-		this.quantity = quantity;
-		this.price = price;
-		setBookValue(price, quantity);
-	}
+
+	//constructors
 	public Stock(){
 		name = "NaN";
 		symbol = "NaN";
@@ -54,7 +49,11 @@ public class Stock {
 		price = 0;
 		bookValue = 0;
 	}
-
+	public Stock(int quantity, float price){
+		this.quantity = quantity;
+		this.price = price;
+		setBookValue(price, quantity);
+	}
 	public Stock(String symbol, String name, int quantity, float price){
 		this.name = name;
 		this.symbol = symbol;
@@ -62,9 +61,27 @@ public class Stock {
 		this.price = price;
 		setBookValue(price, quantity);
 	}
+	
+	//equals
+	public boolean equals(String name, String symbol, int quantity, float price, float bookValue){
+		return 	name.equals(this.name) &&
+				symbol.equals(this.symbol) &&
+				quantity == this.quantity &&
+				Float.compare(price, this.price) == 0 &&
+				Float.compare(bookValue, this.bookValue) == 0;
+	}
 
+	//toString
 	public String toString(){
 		return "Investment: " + symbol + "\nName: " + name + "\nPrice: " + price + "\nQuantity: " + quantity + "\nBook value: " + bookValue;
 	}
 
+	/**
+	 * changes book value and stonk quantity
+	 * @param quantity	new quantity to sub in
+	 */
+	public void sellBookValue(int quantity){
+		bookValue *= ((float)quantity/this.quantity);
+		this.quantity = quantity;
+	}
 }
