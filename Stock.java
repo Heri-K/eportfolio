d1f2f4732171344f31550007185f881dcd7a26e0
ukaparyk <ukaparyk@uoguelph.ca>
2021-10-16 23:30:50 -0400

    constructors and setbookvalue

--- Stock.java
+++ Stock.java
@@ -5,7 +5,7 @@ public class Stock {
 	private String symbol;
 	private int quantity;
 	private float price;
-	private float bookValue;
+	private float bookValue = 0;	//how much to own a stock
 
 	//accessors
 	public String getName(){
@@ -37,5 +37,34 @@ public class Stock {
 	public void setPrice(float price){
 		this.price = price;
 	}
+	public void setBookValue(float price, int quantity){
+		this.bookValue += (price * quantity + 9.99);
+	}
 	
+	//constructor
+	public Stock(int quantity, float price){
+		this.quantity = quantity;
+		this.price = price;
+		setBookValue(price, quantity);
+	}
+	public Stock(){
+		name = "NaN";
+		symbol = "NaN";
+		quantity = 0;
+		price = 0;
+		bookValue = 0;
+	}
+
+	public Stock(String symbol, String name, int quantity, float price){
+		this.name = name;
+		this.symbol = symbol;
+		this.quantity = quantity;
+		this.price = price;
+		setBookValue(price, quantity);
+	}
+
+	public String toString(){
+		return "Investment: " + symbol + "\nName: " + name + "\nPrice: " + price + "\nQuantity: " + quantity + "\nBook value: " + bookValue;
+	}
+
 }
