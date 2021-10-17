6e250f47e90346859d0eceaef141b9e55ada783e
ukaparyk <ukaparyk@uoguelph.ca>
2021-10-16 23:31:07 -0400

    added buyStock and buyMF

--- Portfolio.java
+++ Portfolio.java
@@ -3,78 +3,159 @@ package ukaparyk_a1.eportfolio;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Portfolio{
-	public static void main(String argv[]){
+public class Portfolio {
+	public static void main(String argv[]) {
 		Portfolio here = new Portfolio();
 		ArrayList<Stock> stonks = new ArrayList<Stock>();
 		ArrayList<MutualFund> mutualFunds = new ArrayList<MutualFund>();
-		
 
 		Scanner keyboard = new Scanner(System.in);
 		String command = new String();
-		while (!command.equalsIgnoreCase("quit") && !command.equalsIgnoreCase("q")){
-			System.out.print("Enter the command (available commands are nuy, sell, getGain, search, quit):");
+		while (!command.equalsIgnoreCase("quit") && !command.equalsIgnoreCase("q")) {
+			System.out.print("Enter the command (available commands are buy, sell, getGain, search, quit):");
 			command = keyboard.nextLine();
-			if(!here.validateCommand(command)){
-				System.out.println("Invalid command, try again.");
-				continue;
-			}
-			if (command.equalsIgnoreCase("buy")){
-				here.buyStuff(here, keyboard, stonks, mutualFunds);
+
+			switch(command.toLowerCase().strip()){
+				case "b":
+				case "buy":
+					here.buyStuff(here, keyboard, stonks, mutualFunds);
+					break;
+				default:
+					System.out.println("Invalid command, try again.");
+					break;
 			}
 		}
 		keyboard.close();
 	}
-	/*********************************************************************************/
-	private boolean validateCommand(String command) {							//checks if parameter string matches allowed commands
-		if (command.equalsIgnoreCase("q") || command.equalsIgnoreCase("quit"))	//did it in if/esle statemetns instead of return ...;
-		return true;														//in case i need to add stuff like "q" instead of "quit"
-		else if (command.equalsIgnoreCase("buy"))
-		return true;
-		else if(command.equalsIgnoreCase("sell"))
-		return true;
-		else if(command.equalsIgnoreCase("update"))
-		return true;
-		else if(command.equalsIgnoreCase("getgain"))
-		return true;
-		else if(command.equalsIgnoreCase("search"))
-		return true;
-		return false;
-	}
 
-	private void buyStuff(Portfolio here, Scanner keyboard, ArrayList<Stock> stonks, ArrayList<MutualFund> mutualFunds) {//validatiot for buying stuff
-	System.out.println("Enter the kind of investment(stock or mutualfund), followed by the symbol:");
-	String invType = keyboard.nextLine();
+	/*********************************************************************************/	
+
+	/**
+	 * descriptions of the thing
+	 * @param here	local class references
+	 * @param keyboard	scanner object
+	 * @param stonks	arraylist of stonks
+	 * @param mutualFunds	arraylist of mutual funds
+	 */
+	public void buyStuff(Portfolio here, Scanner keyboard, ArrayList<Stock> stonks,
+			ArrayList<MutualFund> mutualFunds) {// validatiot for buying stuff
+		System.out.println("Enter the kind of investment(stock or mutualfund), followed by the symbol:");
+		String invType = keyboard.nextLine();
 		String[] split = new String[2];
-		while(true){
-			if (invType.isEmpty() || invType.isBlank()){	//checks if the input is blank
+		split = invType.split("[ ]+");
+		while (true) {
+			if (invType.isEmpty() || invType.isBlank()) { // checks if the input is blank
 				System.out.println("Not an investement type, try again.");
 				invType = keyboard.nextLine();
+				split = invType.split("[ ]+");
 				continue;
 			}
-			if(split[1].isEmpty()){	//checks if 2 words were inputted
+			if (!split[0].equalsIgnoreCase("stock") && !split[0].equalsIgnoreCase("mutualfund")) { // checks if investememnt type is valid
+				System.out.println("Not an investement type, try again.");
+				invType = keyboard.nextLine();
+				split = invType.split("[ ]+");
+				continue;
+			}
+			try {
+				if (split[1].isEmpty()) { // checks if 2 words were inputted
+					System.out.println("Symbol missing, try again.");
+					invType = keyboard.nextLine();
+					split = invType.split("[ ]+");
+					continue;
+				}
+			} catch (Exception e) {
 				System.out.println("Symbol missing, try again.");
 				invType = keyboard.nextLine();
-				continue;
-			}
-			if (!split[0].equalsIgnoreCase("stock") || !split[0].equalsIgnoreCase("mutualfund")){	//checks if investememnt type is valid
-				System.out.println("Not an investement type, try again.");
-				invType = keyboard.nextLine();
+				split = invType.split("[ ]+");
 				continue;
 			}
 			break;
 		}
 		if (split[0].equalsIgnoreCase("stock"))
 			here.buyInvStock(stonks, keyboard, split[1]);
-		else if (invType.equalsIgnoreCase("mutualfund"))
+		else if (split[0].equalsIgnoreCase("mutualfund"))
 			here.buyInvMF(mutualFunds, keyboard, split[1]);
 	}
-	
+
 	private void buyInvStock(ArrayList<Stock> stonks, Scanner keyboard, String symbol) {
-		
+		int quantity;
+		System.out.println("Enter the quantity of stock to buy:");
+		while(true){
+			try {
+				quantity = Integer.parseInt(keyboard.nextLine());
+			} catch (Exception e) {
+				System.out.println("Invalid input. Try again.");
+				continue;
+			}
+			break;
+		}
+		float price;
+		System.out.println("Enter the price of the stock to buy:");
+		while(true){
+			try {
+				price = Float.parseFloat(keyboard.nextLine());
+			} catch (Exception e) {
+				System.out.println("Invalid price. Try again.");
+				continue;
+			}
+			break;
+		}
+		for (Stock stock : stonks) {
+			if (symbol.equalsIgnoreCase(stock.getSymbol())) {
+				stonks.add(new Stock(quantity, price));
+				return;
+			}
+		}
+		System.out.println("Enter the name of the stock:");
+		String stockName = keyboard.nextLine();
+		while(true){
+			if (stockName.isBlank() || stockName.isEmpty()){
+				System.out.println("Invalid name, try again.");
+				stockName = keyboard.nextLine();
+			}
+			break;
+		}
+		stonks.add(new Stock(symbol, stockName, quantity, price));
 	}
-	
-	private void buyInvMF(ArrayList<MutualFund> mutualFunds, Scanner keyboard, String split) {
-	
+
+	private void buyInvMF(ArrayList<MutualFund> mutualFunds, Scanner keyboard, String symbol) {
+		int quantity;
+		System.out.println("Enter the quantity of Mutual Fund to buy:");
+		while(true){
+			try {
+				quantity = Integer.parseInt(keyboard.nextLine());
+			} catch (Exception e) {
+				System.out.println("Invalid input. Try again.");
+				continue;
+			}
+			break;
+		}
+		float price;
+		System.out.println("Enter the price of the Mutual Fund to buy:");
+		while(true){
+			try {
+				price = Float.parseFloat(keyboard.nextLine());
+			} catch (Exception e) {
+				System.out.println("Invalid price. Try again.");
+				continue;
+			}
+			break;
+		}
+		for (MutualFund mFund: mutualFunds) {
+			if (symbol.equalsIgnoreCase(mFund.getSymbol())) {
+				mutualFunds.add(new MutualFund(quantity, price));
+				return;
+			}
+		}
+		System.out.println("Enter the name of the mutual fund:");
+		String stockName = keyboard.nextLine();
+		while(true){
+			if (stockName.isBlank() || stockName.isEmpty()){
+				System.out.println("Invalid name, try again.");
+				stockName = keyboard.nextLine();
+			}
+			break;
+		}
+		mutualFunds.add(new MutualFund(symbol, stockName, quantity, price));
 	}
 }
